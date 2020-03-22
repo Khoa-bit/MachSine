4ec9378e7b151adfafa42e54f2f14dc060310780
Khoa-Bit <55542012+Khoa-bit@users.noreply.github.com>
2020-03-22 17:07:10 +0700

    Develop terminal UI

--- cashier2.0.py
+++ cashier2.0.py
@@ -1,73 +1,27 @@
-import json
+from machSine import MachSine
 
-filename = 'wallet.json'
-with open(filename) as f:
-    wallet = json.load(f)
+# Make an instance
+machsine = MachSine()
 
-money = {"20_000": 0, "10_000": 0, "5_000": 0, "2_000": 0, "1_000": 0, "500": 0}
-candy = False
+while True:
+    """Main Loop"""
+    print()
+    print("====================MACHSINE v2.0====================")
+    print("Please type 1 or 2 to choose your action:")
+    print("(type any letter to quit.)")
+    print("1. Change money")
+    print("2. Open wallet (Staff only")
+    print("3. Restock wallet (Staff only)")
 
-# Prompt cashier
-print("Change money machSine")
-give =  int(input("Take:"))
-price = int(input("Cost:"))
-change = 0
+    action = input("Input: ")
+    print()
 
-# Post change
-if (give > price):
-    change = give - price
-    print(f"Original change: {change}")
-
-    # Calculate change with existing amount
-    while(change != 0):
-        if (change >= 20_000 and wallet["20_000"] > 0):
-            change -= 20_000
-            wallet["20_000"] -= 1
-            money["20_000"] += 1
-
-        elif (change >= 10_000 and wallet["10_000"] > 0):
-            change -= 10_000
-            wallet["10_000"] -= 1
-            money["10_000"] += 1
-            
-        elif (change >= 5_000 and wallet["5_000"] > 0):
-            change -= 5_000
-            wallet["5_000"] -= 1
-            money["5_000"] += 1
-
-        elif (change >= 2_000 and wallet["2_000"] > 0):
-            change -= 2_000
-            wallet["2_000"] -= 1
-            money["2_000"] += 1
-
-        elif (change >= 1_000 and wallet["1_000"] > 0):
-            change -= 1_000
-            wallet["1_000"] -= 1
-            money["1_000"] += 1
-
-        elif (change >= 500 and wallet["500"] > 0):
-            change -= 500
-            wallet["500"] -= 1
-            money["500"] += 1
-
-        else:
-            candy = True
-            break
-
-    # Give change to customer
-    print("Change:")
-
-    for currency, amount in money.items():
-        if amount != 0:
-            print(f"{currency}vnd x{amount}")
-
-    if candy:
-        print(f"Candy instead? ({change}vnd left)")
-
-elif (give == price):
-    print("Thank you! See you soon.")
-else:
-    print("Not enough money.")
-
-with open(filename, 'w') as f:
-    json.dump(wallet, f)
+    if action == "1":
+        print("====================CHANGE MONEY====================")
+        machsine.change_money()
+    elif action == "2":
+        pass
+    elif action == "3":
+        pass
+    else:
+        break
---
+++ machSine.py
@@ -0,0 +1,91 @@
+import json
+
+class MachSine:
+    """A class to represent a MachSine."""
+    def __init__(self):
+        # Load wallet
+        filename = 'wallet.json'
+        with open(filename) as f:
+            self.wallet = json.load(f)
+
+    def change_money(self):
+        # Initialize change
+        money = {"20_000": 0, "10_000": 0, "5_000": 0, "2_000": 0, "1_000": 0, "500": 0}
+        candy = False
+
+        # Prompt user
+        give =  int(input("Take:"))
+        price = int(input("Cost:"))
+        change = 0
+
+        # Post change
+        if (give > price):
+            change = give - price
+            print(f"Original change: {change}")
+
+            # Calculate change with existing amount
+            while(change != 0):
+                if (change >= 20_000 and self.wallet["20_000"] > 0):
+                    change -= 20_000
+                    self.wallet["20_000"] -= 1
+                    money["20_000"] += 1
+
+                elif (change >= 10_000 and self.wallet["10_000"] > 0):
+                    change -= 10_000
+                    self.wallet["10_000"] -= 1
+                    money["10_000"] += 1
+                    
+                elif (change >= 5_000 and self.wallet["5_000"] > 0):
+                    change -= 5_000
+                    self.wallet["5_000"] -= 1
+                    money["5_000"] += 1
+
+                elif (change >= 2_000 and self.wallet["2_000"] > 0):
+                    change -= 2_000
+                    self.wallet["2_000"] -= 1
+                    money["2_000"] += 1
+
+                elif (change >= 1_000 and self.wallet["1_000"] > 0):
+                    change -= 1_000
+                    self.wallet["1_000"] -= 1
+                    money["1_000"] += 1
+
+                elif (change >= 500 and self.wallet["500"] > 0):
+                    change -= 500
+                    self.wallet["500"] -= 1
+                    money["500"] += 1
+
+                else:
+                    candy = True
+                    break
+
+            # Check whether machine have enough money in wallet to give change.
+            if (change < 500):
+                # Give change to customer
+                print("Change:")
+
+                for currency, amount in money.items():
+                    if amount != 0:
+                        print(f"{currency}vnd x{amount}")
+
+                if candy:
+                    print(f"Candy instead? ({change}vnd left)")
+
+            else:
+                print("Cannot give change... :(")
+                print("Sorry for this inconvenience. Please use less money or contact our staff for help.")
+
+
+        elif (give == price):
+            print("Perfect! See you soon. :)")
+        elif (give < price):
+            change = price - give
+            print(f"Sorry... You are missing {change}vnd.")
+
+        # Update wallet.
+        filename = 'wallet.json'
+        with open(filename, 'w') as f:
+            json.dump(self.wallet, f)
+
+        print()
+        input("Press any key to continue.")
